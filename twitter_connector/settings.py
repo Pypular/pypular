361a3ff91484b79076d66195b6c3e3e6446b21e7
Fábio Cerqueira <cerqueirasfabio@gmail.com>
2016-11-25 19:53:55 +0000

    Change settings approach
    Added [simple-settings](http://simple-settings.readthedocs.io/)

---
+++ twitter_connector/settings.py
@@ -0,0 +1,13 @@
+import os
+
+
+def get_config(environ_key_name, default_value, convert=str):
+    return convert(os.environ.get(environ_key_name, default_value))
+
+
+TWITTER_CONFIG = {
+    'CONSUMER_KEY': get_config('TW_CONSUMER_KEY', ''),
+    'CONSUMER_SECRET': get_config('TW_CONSUMER_SECRET', ''),
+    'ACCESS_TOKEN': get_config('TW_ACCESS_TOKEN', ''),
+    'ACCESS_TOKEN_SECRET': get_config('TW_ACCESS_TOKEN_SECRET', ''),
+}
