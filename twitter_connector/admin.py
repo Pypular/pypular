c9fc3a6d538ead4c7aad1f3101c8e8f1e321ce4d
Lucas <lffsantos@gmail.com>
2016-12-03 23:26:58 +0000

    added filter

--- twitter_connector/admin.py
+++ twitter_connector/admin.py
@@ -15,22 +15,31 @@ class HashTagUrlInline(admin.TabularInline):
 class TweetModelAdmin(admin.ModelAdmin):
     inlines = [UrlInline]
     list_display = ['text', 'created_at', 'retweet_count', 'favorite_count']
+    search_fields = ['text']
+    list_filter = ['created_at']
 
 
 class UrlModelAdmin(admin.ModelAdmin):
     inlines = [HashTagUrlInline]
+    list_display = ['url', 'expanded_url']
+    search_fields = ['url', 'expanded_url']
 
 
 class HashTagUrlAdmin(admin.ModelAdmin):
     list_display = ['hashtag', 'url']
+    search_fields = ['hashtag', 'url']
 
 
 class TweetUrlAdmin(admin.ModelAdmin):
     list_display = ['tweet', 'url']
+    search_fields = ['tweet', 'url']
 
+
+class HastagModelAdmin(admin.ModelAdmin):
+    search_fields = ['hashtag']
 
 admin.site.register(Url, UrlModelAdmin)
-admin.site.register(Hashtag)
+admin.site.register(Hashtag, HastagModelAdmin)
 admin.site.register(Tweet, TweetModelAdmin)
 admin.site.register(HashtagUrl, HashTagUrlAdmin)
 admin.site.register(TweetUrl, TweetUrlAdmin)
